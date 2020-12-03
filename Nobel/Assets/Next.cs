b6ee22d58d5ead63577835dd93af73bfc9a61949
murata0531 <coless115@gmail.com>
2020-12-04 04:15:20 +0900

    commit

---
+++ Nobel/Assets/Next.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utage;
+
+public class Next : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void buttonpush()
+    {
+       SceneManager.LoadScene ("Setumei2"); 
+    }
+}
